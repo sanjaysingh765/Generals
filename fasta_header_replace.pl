f02bbe72c4dece0192de81d33b9585473ad902ac
Sanjay Singh <sanjaysingh765@users.noreply.github.com>
2019-08-26 12:12:39 -0400

    Update fasta_header_replace.pl

--- fasta_header_replace.pl
+++ fasta_header_replace.pl
@@ -8,18 +8,15 @@ my $header = <STDIN>;
 open(my $fh_annotations, $header) or die "Can't open $header";
 my @annotfile = <$fh_annotations>;
 close $fh_annotations;
-
 # file where output will be saved 
 my $result = 'result.fasta';
 open(RESULT, '>', $result);
-
 # Read the sequence file
 print"Enter sequence file...\n";
 my $seq = <STDIN>;
 open(my $fh_genes, $seq) or die "Can't open $seq";
 my @seqfile = <$fh_genes>;
 close $fh_genes;
-
 # Process the header data
 my %names; 
 foreach my $line (@annotfile) {
@@ -27,7 +24,6 @@ foreach my $line (@annotfile) {
   my @fields = split /\t/, $line;   
   $names{$fields[0]} = $fields[1];  
 }
-
 # Process the sequence data
 foreach my $line (@seqfile) {
   if ($line =~ m/>(.+)$/) {
@@ -35,7 +31,6 @@ foreach my $line (@seqfile) {
        $line =~ s/($1)/$names{$1}/;
       }
   }
-
 print RESULT $line;
 
 }
